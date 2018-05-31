ac56ad7ef9107308aba4067c65dd516a804aa01d
Your Name <you@example.com>
2018-05-31 22:13:43 +0800

    koa

--- koa/application.js
+++ koa/application.js
@@ -5,12 +5,13 @@ let response = require('./response');
 class Koa {
   constructor(){
     this.callbackFn;
+    this.middlewares = [];
     this.context = context;
     this.request = request;
     this.response = response;
   }
   use(cb){
-    this.callbackFn = cb;
+    this.middlewares.push(cb);
   }
   createContext(req,res){
     //  Object.create方法的作用
@@ -21,16 +22,30 @@ class Koa {
     ctx.res =ctx.response.res =  res;
     return ctx; // 返回上下文对象
   }
+  compose(ctx,middlewares){
+    function dispatch(index) {
+      // 越界说明都执行完毕了
+      if (index === middlewares.length ) return Promise.resolve();
+      let middleware = middlewares[index];
+      // 递归创建 套起来的promise
+      return Promise.resolve(middleware(ctx,()=>dispatch(index+1)));
+    }
+    return dispatch(0);
+  }
   handleRequest(req,res){
     res.statusCode = 404;// 默认页面找不到
     let ctx = this.createContext(req,res);
-    this.callbackFn(ctx);// 当回调函数执行后，ctx.body值就会发生变化
-    let body = ctx.body;
-    if(typeof body === 'undefined'){
-      res.end(`Not Found`);
-    }else if(typeof body === 'string'){
-      res.end(body);
-    }
+    let composeMiddleware = this.compose(ctx,this.middlewares);
+    // 当回调函数执行后，ctx.body值就会发生变化
+    // 当此promise执行完后 在去res.end();
+    composeMiddleware.then(()=>{
+      let body = ctx.body;
+      if (typeof body === 'undefined') {
+        res.end(`Not Found`);
+      } else if (typeof body === 'string') {
+        res.end(body);
+      }
+    });
   }
   listen(){
     let server = http.createServer(this.handleRequest.bind(this));
